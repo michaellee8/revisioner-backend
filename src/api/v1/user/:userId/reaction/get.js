0cf8187205f6e4de59e6c7378d13a64ad685efce
Michael Lee <ckmichael8@gmail.com>
2017-09-01 23:10:44 +0800

    Implement user/reaction/get

--- src/api/v1/user/:userId/reaction/get.js
+++ src/api/v1/user/:userId/reaction/get.js
@@ -1,5 +1,28 @@
 var db: mysql$Connection = require("projectDb");
 
 module.exports = function(req: express$Request, res: express$Response) {
-  db.query;
+  if (!req.query.targetserId) {
+    res.status(400).send({ error: "Missing parameter targetUserId" });
+  } else {
+    db.query(
+      `
+    SELECT COUNT(*) AS userReactionCount, userReactionType, targetUserId
+    FROM revisioner.userReactions
+    WHERE targetUserId = ?
+    GROUP BY userReactionType
+    ORDER BY COUNT(*) DESC
+  `,
+      [req.query.targetUserId],
+      (error, results, fields): mysql$QueryCallback => {
+        if (error) {
+          console.log(error);
+          res
+            .status(500)
+            .send({ error: "Database query error, please try again later" });
+        } else {
+          res.send(results);
+        }
+      }
+    );
+  }
 };
